2c40d87ddc122a61dacc55d16d20fa3884eb1d91
muschellij2 <muschellij2@gmail.com>
2016-10-24 17:07:34 -0400

    Adding HCP ids.

---
+++ R/hcp_ids.R
@@ -0,0 +1,28 @@
+#' @title Get list of HCP Ids
+#' @description Returns list of IDs of folders in the HCP database
+#' @param group Group of IDs to return
+#'
+#' @return Character vector
+#' @export
+#'
+#' @examples
+#' if (have_aws_key()) {
+#' head(hcp_ids("HCP"))
+#' head(hcp_ids("HCP_900"))
+#' }
+hcp_ids = function(group = c("HCP", "HCP_900")) {
+  group = match.arg(group)
+
+  prefix = paste0(group, "/")
+  files = hcp_list_files(prefix = prefix, delimiter = "/")
+  pc = parse_list_files(files)
+  pc = pc$prefixes
+  n = colnames(pc)
+  n = n %in% "CommonPrefixes"
+  pc = pc[, n]
+  pc = unlist(pc)
+  names(pc) = NULL
+  pc = gsub(prefix, "", pc, fixed = TRUE)
+  pc = sub("/$", "", pc)
+  retur(pc)
+}
