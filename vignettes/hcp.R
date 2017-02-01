235f2d94c4288122dcf2993ab0510c44439d1539
muschellij2 <muschellij2@gmail.com>
2017-02-01 13:33:36 -0500

    Fixing vignette.

--- vignettes/hcp.R
+++ vignettes/hcp.R
@@ -11,5 +11,37 @@ knitr::opts_chunk$set(echo = TRUE, cache = FALSE, comment = "")
 #  set_aws_api_key(access_key = "ACCESS_KEY", secret_key = "SECRET_KEY")
 
 ## ----blist_show, eval = FALSE--------------------------------------------
-#  hcp::bucketlist()
+#  if (have_aws_key()) {
+#    hcp::bucketlist()
+#  }
+
+## ----blist_go, echo = FALSE----------------------------------------------
+if (have_aws_key()) {
+  hcp::bucketlist(verbose = FALSE)
+}
+
+## ---- eval = TRUE--------------------------------------------------------
+ids_with_dwi = hcp_900_scanning_info %>% 
+  filter(scan_type %in% "dMRI") %>% 
+  select(id) %>% 
+  unique
+head(ids_with_dwi)
+
+## ---- eval = FALSE, echo = TRUE------------------------------------------
+#  r = download_hcp_dir("HCP/100307/T1w/Diffusion")
+#  print(basename(r$output_files))
+
+## ---- eval = TRUE, echo = FALSE------------------------------------------
+r = list(output_files = c("bvals", "bvecs", "data.nii.gz", "grad_dev.nii.gz", "nodif_brain_mask.nii.gz")
+)
+r$output_files
+
+## ---- eval = FALSE-------------------------------------------------------
+#  ids_with_dwi = ids_with_dwi %>%
+#    mutate(id_dir = paste0("HCP/", id, "/T1w/Diffusion"))
+
+## ----dl_file-------------------------------------------------------------
+if (have_aws_key()) {
+  ret = download_hcp_file("HCP/100307/T1w/Diffusion/bvals")
+}
 
